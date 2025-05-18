04f658992563acd4851c2c5d2f71fe2135832d9f
Leopold <leopolddag@gmail.com>
2025-05-18 16:26:48 +0200

    new back added

---
+++ paymybuddyback/src/main/java/com/openclassrooms/paymybuddyback/PaymybuddybackApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paymybuddyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddybackApplication.class, args);
+	}
+
+}
---
+++ paymybuddyback/src/test/java/com/openclassrooms/paymybuddyback/PaymybuddybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paymybuddyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
