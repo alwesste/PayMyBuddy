458bed6cbaf9517a2ec012cacd65dd646684801c
Leopold <leopolddag@gmail.com>
2025-05-18 15:18:43 +0200

    Initialisation du back PayMyBuddy-back

---
+++ paybybuddy-back/src/main/java/com/openclassrooms/paybybuddy_back/PaybybuddyBackApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paybybuddy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaybybuddyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaybybuddyBackApplication.class, args);
+	}
+
+}
---
+++ paybybuddy-back/src/test/java/com/openclassrooms/paybybuddy_back/PaybybuddyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paybybuddy_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaybybuddyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
