c0189364ab95e9c6e9cf839d367fb80d1857baa1
Leopold <leopolddag@gmail.com>
2025-06-27 14:26:42 +0200

    Creation du formulaire pour le profil avec changement du mot de passe ci-besoin

--- paymybuddy-front/src/pages/Profil/Profil.tsx
+++ paymybuddy-front/src/pages/Profil/Profil.tsx
@@ -1,16 +1,81 @@
-import React from "react";
+import React, {useState} from "react";
 import './Profil.scss'
 import Layout from "../../components/Layout/layout.tsx";
 
 const Profil: React.FC = () => {
-    return(
-        <Layout>
-            <div className="profil-container">
-                <p>Username</p>
-                <p>Mail</p>
-                <p>Mot de passe</p>
-            </div>
 
+    const currentUserEmail: string | null = localStorage.getItem("currentUserEmail");
+    const currentUserPassword: string | null = localStorage.getItem("currentUserPassword");
+    const [formData, setFormData] = useState({
+        username: "",
+        email: currentUserEmail ?? "",
+        password: currentUserPassword ?? "",
+    })
+
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData, [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log("formData:", formData)
+
+        try {
+            const response = await fetch("http://localhost:8080/api/updatePassword", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                throw new Error(errorText || "Échec de la mise à jour.");
+            }
+        } catch (error ) {
+            console.error("Erreur :", error);
+        }
+    };
+
+    return (
+        <Layout>
+            <form action="" className="profil-container" onSubmit={handleSubmit}>
+                <div  className="profil-container_form">
+                    <label htmlFor="username">Username</label>
+                    <input type="text"
+                           id="username"
+                           name="username"
+                           value={formData.username}
+                           onChange={handleChange}
+                           placeholder="Votre username"/>
+                </div>
+
+                <div className="profil-container_form">
+                    <label htmlFor="mail">Mail</label>
+                    <input type="email"
+                           id="email"
+                           name="email"
+                           value={formData.email}
+                           onChange={handleChange}
+                           placeholder="Votre mail"/>
+                </div>
+
+                <div className="profil-container_form">
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password"
+                           id="password"
+                           name="password"
+                           value={formData.password}
+                           onChange={handleChange}
+                           placeholder="Votre mot de passe"/>
+                </div>
+
+
+                <button type="submit">Modifier</button>
+            </form>
         </Layout>
     )
 }
